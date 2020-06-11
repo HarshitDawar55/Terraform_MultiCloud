707d00d5550e09363a78cfa59844c32748911215
Harshit Dawar <31154755+HarshitDawar55@users.noreply.github.com>
2020-06-11 21:42:30 +0530

    File for Server Testing !

---
+++ index.php
@@ -0,0 +1,15 @@
+<body bgcolor = "aqua">
+
+<pre>
+
+<?php
+
+print "Welcome to Webserver by HarshitDawar deployed using End-to-End Pipeline containing Terraform, Github, & AWS Tools!";
+
+print `ifconfig`;
+
+print `free -m`;
+
+?>
+
+</pre>
